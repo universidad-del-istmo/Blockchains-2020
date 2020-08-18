d213b84e887521abc1ac3eeadfb1350b0c52b5a5
Miguel Gonzalez <miguelgon_8@hotmail.com>
2020-08-18 17:05:04 -0600

    Verification enabled

--- ejercicios/RSA/entregas/Miguel González/Ejercicio RSA/encripcion.py
+++ ejercicios/RSA/entregas/Miguel González/Ejercicio RSA/encripcion.py
@@ -5,89 +5,35 @@ import cryptomath
 import rabinMiller as number
 from hashlib import sha256
 
-keysPrefix = "ejercicios\RSA\entregas\Miguel González\Ejercicio RSA\Ej_RSA"
+# Directorio en donde se van a guardar las llaves
+keysDir = "ejercicios\RSA\entregas\Miguel González\Ejercicio RSA\Ej_RSA"
+keySize = 8
 
 
 def main():
     print("Creating keys...")
     # Al utilizar llaves muy grandes tarda bastante tiempo en encriptar
-    createKeys(keysPrefix, 64)
+    createKeyFiles(keysDir, keySize)
 
+    # Encriptar
     messageEnc = input("\nNumber to encrypt: ")
     print("Encrypting number...")
     encrypt(messageEnc)
 
+    # Decriptar
     messageDec = input("\nNumber to decrypt: ")
     print("Decrypting number...")
     decrypt(messageDec)
 
-    print("Signing...")
+    # Firmar
+    print("\nSigning...")
     signature, Hash = sign(
         "ejercicios\RSA\entregas\Miguel González\Ejercicio RSA\sign.txt")
 
-    print("Verifying...")
+    # Verificar firma
+    print("\nVerifying...")
     # Hay un problema con la verificacion, creo que se debe a la longitud de la llave
     verify(signature, Hash)
-
-
-def encrypt(message):
-    message = int(message)
-    pubKey = open(keysPrefix + "_publicKey.txt").read()
-    pubKey = pubKey.split(",")
-    n = pubKey[1]
-    n = int(n)
-    e = pubKey[2]
-    e = int(e)
-    encrypted = (message ** e) % n
-    print("Encrypted number: ", encrypted)
-    return encrypted
-
-
-def decrypt(encrypted):
-    encrypted = int(encrypted)
-    privKey = open(keysPrefix + "_privateKey.txt").read()
-    privKey = privKey.split(",")
-    n = privKey[1]
-    n = int(n)
-    d = privKey[2]
-    d = int(d)
-    decrypted = (encrypted ** d) % n
-    print("Decrypted number: ", decrypted)
-    return decrypted
-
-
-def sign(fileDir):
-    privKey = open(keysPrefix + "_privateKey.txt").read()
-    privKey = privKey.split(",")
-    message = open(fileDir).read()
-    n = privKey[1]
-    n = int(n)
-    d = privKey[2]
-    d = int(d)
-    message = bytes(message, 'utf-8')
-    hashed = sha256(message).digest()
-    hashed = int.from_bytes(hashed, byteorder='big')
-    print("Signing hash: ", hex(hashed))
-    signature = (hashed**d) % n
-    print("Signature:", hex(signature))
-    return signature, hashed
-
-
-def verify(signature, hashFromSignature):
-    pubKey = open(keysPrefix + "_publicKey.txt").read()
-    pubKey = pubKey.split(",")
-    n = pubKey[1]
-    n = int(n)
-    e = pubKey[2]
-    e = int(e)
-    Hash = (signature**e) % n
-    print("Hash:", hex(Hash))
-    print("Signature valid:", Hash == hashFromSignature)
-    return Hash
-
-    # hash = int.from_bytes(sha512(messageEnc).digest(), byteorder='big')
-    # signature = pow(hash, d, n)
-    # print("Signature:", hex(signature))
 
 
 def generateKeys(keySize):
@@ -113,7 +59,7 @@ def generateKeys(keySize):
     return (publicKey, privateKey)
 
 
-def createKeys(name, keySize):
+def createKeyFiles(name, keySize):
     # Creacion de los archivos de la llave publica
     publicKey, privateKey = generateKeys(keySize)
 
@@ -126,6 +72,56 @@ def createKeys(name, keySize):
     fo = open("%s_privateKey.txt" % (name), "w")
     fo.write("%s,%s,%s" % (keySize, privateKey[0], privateKey[1]))
     fo.close()
+
+
+def encrypt(message):
+    message = int(message)
+    pubKey = open(keysDir + "_publicKey.txt").read()
+    pubKey = pubKey.split(",")
+    n = int(pubKey[1])
+    e = int(pubKey[2])
+    encrypted = (message ** e) % n
+    print("Encrypted number: ", encrypted)
+    return encrypted
+
+
+def decrypt(encrypted):
+    encrypted = int(encrypted)
+    privKey = open(keysDir + "_privateKey.txt").read()
+    privKey = privKey.split(",")
+    n = int(privKey[1])
+    d = int(privKey[2])
+    decrypted = (encrypted ** d) % n
+    print("Decrypted number: ", decrypted)
+    return decrypted
+
+
+def sign(fileDir):
+    privKey = open(keysDir + "_privateKey.txt").read()
+    privKey = privKey.split(",")
+    message = open(fileDir).read()
+    n = int(privKey[1])
+    d = int(privKey[2])
+    message = bytes(message, 'utf-8')
+    hashed = sha256(message).digest()
+    hashed = int.from_bytes(hashed, byteorder='big')
+    # Tomar una pequeña parte del hash para realizar la verificacion
+    hashed = int(str(hashed)[:5])
+    print("Signing hash: ", (hashed))
+    signature = (hashed**d) % n
+    print("Signature:", (signature))
+    return signature, hashed
+
+
+def verify(signature, hashFromSignature):
+    pubKey = open(keysDir + "_publicKey.txt").read()
+    pubKey = pubKey.split(",")
+    n = int(pubKey[1])
+    e = int(pubKey[2])
+    Hash = (signature**e) % n
+    print("Hash:", (Hash))
+    print("Signature valid? ", Hash == hashFromSignature)
+    return Hash
 
 
 if __name__ == "__main__":
