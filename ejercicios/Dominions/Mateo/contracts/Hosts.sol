6ace3e5074012e853d9d554154ef127e931c3c17
mint <marcosmateom@gmail.com>
2020-09-30 00:09:41 -0600

    Listo(casi)

--- ejercicios/Dominions/Mateo/contracts/Hosts.sol
+++ ejercicios/Dominions/Mateo/contracts/Hosts.sol
@@ -1,9 +1,13 @@
 pragma solidity >=0.5.0 <0.8.0;
 contract Hosts{
-
+    
+    mapping (address => uint) balances; 
     address payable public reguladora;
+    event ipCanged(string domainName, string ip);
+    event pujaRealizada(string domainName, uint amount, address comprador);
     constructor() public{
         reguladora = msg.sender;
+        balances[msg.sender] = 10000000000000; 
     }
     struct boughtHost{
         address payable dueno;
@@ -37,11 +41,20 @@ contract Hosts{
     function setPuja(address payable _comprador, address payable _vendedor, string memory _dominio, uint _monto) public {
         numPuj++;
         puja storage c = pujas[numPuj];
-        c.comprador = _comprador;
-        c.vendedor = _vendedor;
-        c.dominio = _dominio;
-        c.monto = _monto;
+        if(_vendedor != address(0)){
+            c.comprador = _comprador;
+            c.vendedor = _vendedor;
+            c.dominio = _dominio;
+            c.monto = _monto;    
+        } else {
+            c.comprador = _comprador;
+            c.vendedor = reguladora;
+            c.dominio = _dominio;
+            c.monto = _monto;
+        }
+        balances[_comprador] -=_monto;
         pujaID.push(numPuj) -1;
+        emit pujaRealizada(_dominio, _monto, _comprador);
 
     }
     
@@ -57,16 +70,48 @@ contract Hosts{
         return (boughtHosts[ins].ip, boughtHosts[ins].dominio);
     }
 
-    function getPuja(uint ins) view public returns (address, string memory, uint) {
-        return (pujas[ins].comprador, pujas[ins].dominio, pujas[ins].monto);
+    function getPuja(uint ins) view public returns (address, address, string memory, uint) {
+        return (pujas[ins].comprador, pujas[ins].vendedor, pujas[ins].dominio, pujas[ins].monto);
     }
     
     function aceptarPuja(uint pujaAceptada) public returns (address, string memory, uint){
+        require(pujas[pujaAceptada].vendedor == msg.sender, "El dominio no le pertenese, usted no puede aceptar la puja");
         setHost(pujas[pujaAceptada].comprador,"",pujas[pujaAceptada].dominio);
+        sendCoin(pujas[pujaAceptada].comprador, pujas[pujaAceptada].vendedor, pujas[pujaAceptada].monto);
     }
+    
+    function rechazarPuja(uint pujaRechazada) public{
+        require(pujas[pujaRechazada].vendedor == msg.sender, "El dominio no le pertenese, usted no puede aceptar la puja");
+        balances[pujas[pujaRechazada].comprador] +=pujas[pujaRechazada].monto;
+        delete pujas[pujaRechazada];
+        
+    }
+    
+    function cancelarPuja(uint pujaCancelada) public{
+        require(pujas[pujaCancelada].comprador == msg.sender, "Usted no realizo la puja, por consiguiente no la puede rechazar");
+        balances[pujas[pujaCancelada].comprador] +=pujas[pujaCancelada].monto;
+        delete pujas[pujaCancelada];
+    }
+    
     function setIp(string memory _ip) public {
         boughtHost storage c = boughtHosts[msg.sender];
         require(msg.sender==c.dueno, "Usted no es el dueno del dominio");
         c.ip = _ip;
+        emit ipCanged(c.dominio, _ip);
+    }
+    
+    function addMoney(address payable recipient, uint monto) public{
+        balances[recipient] +=monto;
+    }
+    
+    function getBalance(address _addr) public view returns(uint) { 
+        return balances[_addr];
+    }
+    
+    function sendCoin(address _sender, address _receiver, uint _amount) public returns(bool sufficient) {
+        if (balances[_sender] < _amount) return false;
+        balances[_sender] -= _amount;
+        balances[_receiver] += _amount;
+        return true;
     }
 }
