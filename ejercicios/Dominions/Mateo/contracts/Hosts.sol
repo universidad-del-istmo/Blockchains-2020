0ea8ee1d66d711d374e1b18cdb35a3a7211e0f1c
mint <marcosmateom@gmail.com>
2020-09-29 22:41:07 -0600

    Ya se pueden aceptar pujas y setiar ip

--- ejercicios/Dominions/Mateo/contracts/Hosts.sol
+++ ejercicios/Dominions/Mateo/contracts/Hosts.sol
@@ -10,23 +10,63 @@ contract Hosts{
         string ip;
         string dominio;
     }
+
+    struct puja{
+        address payable comprador;
+        address payable vendedor;
+        string dominio;
+        uint monto;
+    }
+
     mapping(address => boughtHost) public boughtHosts;
     address[] public boughtHostAccts;
-    
-    function setHost(address _address, string memory _ip, string memory _dominio) public {
-        boughtHost storage c = boughtHosts[_address];
 
+    mapping(uint => puja) public pujas;
+    uint[] public pujaID;
+    uint numPuj =0;
+    
+    function setHost(address payable _address, string memory _ip, string memory _dominio) public {
+        boughtHost storage c = boughtHosts[_address];
+        c.dueno = _address;
         c.ip = _ip;
-        c.dominio = _dominio;       
+        c.dominio = _dominio;
         boughtHostAccts.push(_address) -1;
 
     }
-    
-    function getboughtHosts() view public returns (address[] memory) {
-        return boughtHostAccts;
+
+    function setPuja(address payable _comprador, address payable _vendedor, string memory _dominio, uint _monto) public {
+        numPuj++;
+        puja storage c = pujas[numPuj];
+        c.comprador = _comprador;
+        c.vendedor = _vendedor;
+        c.dominio = _dominio;
+        c.monto = _monto;
+        pujaID.push(numPuj) -1;
+
     }
     
-    function getboughtHost(address ins) view public returns (string memory, string memory) {
+    function getBoughtHosts() view public returns (address[] memory) {
+        return boughtHostAccts;
+    }
+
+    function getPujas() view public returns (uint[] memory) {
+        return pujaID;
+    }
+    
+    function getBoughtHost(address ins) view public returns (string memory, string memory) {
         return (boughtHosts[ins].ip, boughtHosts[ins].dominio);
+    }
+
+    function getPuja(uint ins) view public returns (address, string memory, uint) {
+        return (pujas[ins].comprador, pujas[ins].dominio, pujas[ins].monto);
+    }
+    
+    function aceptarPuja(uint pujaAceptada) public returns (address, string memory, uint){
+        setHost(pujas[pujaAceptada].comprador,"",pujas[pujaAceptada].dominio);
+    }
+    function setIp(string memory _ip) public {
+        boughtHost storage c = boughtHosts[msg.sender];
+        require(msg.sender==c.dueno, "Usted no es el dueno del dominio");
+        c.ip = _ip;
     }
 }
